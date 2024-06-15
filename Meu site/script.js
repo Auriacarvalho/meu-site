05b3e1ef8537df1d89b8e4eb07b4bdc82c0f68f2
AuriaCarvalho <165321798+Auriacarvalho@users.noreply.github.com>
2024-06-15 11:46:02 -0300

    Add files via upload

---
+++ Meu site/script.js
@@ -0,0 +1,22 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`);
+});
+
+// Inicialização do Swiper
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
